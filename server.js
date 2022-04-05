1b3e8ef7bd21b55b6fa92f70c2bc8a55bed506d1
Gustavo Tijerino <busg26@gmail.com>
2022-04-04 22:33:47 -0500

    Updated server.js

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
-console.log("hi")
-const express = require('express')
-const app = express()
-app.listen(3000)
 
-// app.get('/' (req, res)) ()=> {
+const express = require('express');
+const { dirname } = require('path');
+const app = express()
+const path = require('path');
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log("Hi"))
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static("public"))
+
+app.get('/notes', (req, res)  => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
     
-// }
+})
