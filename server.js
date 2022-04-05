8eb658955e6c2f94c23a5d97c0ab8f596f366b1e
Gustavo Tijerino <busg26@gmail.com>
2022-04-05 09:21:07 -0500

    Added more variables to server

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 
 const express = require('express');
-const { dirname } = require('path');
 const app = express()
 const path = require('path');
+const uuid = require("uuid");
+const db = require('./db/db.json')
 
 const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log("Hi"))
+app.listen(PORT, console.log("Hi"))
 
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 // app.use(express.static("public"))
@@ -15,3 +17,19 @@ app.get('/notes', (req, res)  => {
     res.sendFile(path.join(__dirname, 'public/notes.html'))
     
 })
+
+app.get("/api/notes", (req, res) => res.json(notes))
+
+
+// Save user Inputs
+// delete old inputs
+
+
+// app.post("/api/notes", (req, res) => {
+//     let newNote = "hi"
+//     console.log("newNote")
+
+
+//    db.push(newNote);
+//   res.json(db);
+// })
