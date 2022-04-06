55d7d3a9d10b97595bfa54e3ebad940284ad3e77
Gustavo Tijerino <busg26@gmail.com>
2022-04-05 23:28:02 -0500

    Updated Server

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 
 const express = require('express');
-const app = express()
 const path = require('path');
 const uuid = require("uuid");
 const db = require('./db/db.json')
+const fs = require('fs');
 
+
+const app = express()
 const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log("Hi"))
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
@@ -17,19 +18,33 @@ app.get('/notes', (req, res)  => {
     res.sendFile(path.join(__dirname, 'public/notes.html'))
     
 })
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, ('db/db.json')))
+})
 
-app.get("/api/notes", (req, res) => res.json(notes))
-
+app.get('*', (req, res)  => {
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+    
+})
+app.post('/api/notes', (req, res)  => {
+   const currentNotes = JSON.parse(fs.readFileSync('db/db.json'))
+   let newNote = req.body
+   currentNotes.push(newNote)
+   fs.writeFileSync('db/db.json',JSON.stringify(currentNotes))
+   res.json(currentNotes)
+    
+})
 
 // Save user Inputs
 // delete old inputs
 
 
-// app.post("/api/notes", (req, res) => {
-//     let newNote = "hi"
-//     console.log("newNote")
 
 
-//    db.push(newNote);
-//   res.json(db);
-// })
+
+
+
+
+
+
+app.listen(PORT, console.log("Hi"))
